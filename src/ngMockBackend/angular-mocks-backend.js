eac269912333270710380eb27fb586d5c9f8f80a
Erko Bridee <erko.bridee@gmail.com>
2014-10-01 00:06:37 -0300

    comment updated
    next version number

--- src/ngMockBackend/angular-mocks-backend.js
+++ src/ngMockBackend/angular-mocks-backend.js
@@ -1,6 +1,6 @@
 /*
-  AngularJS Mocks Backend v0.1.4
-  (c) 2014 Erko Bridee - https://github.com/erkobridee/angular-mocks-backend/releases/tag/v0.1.4
+  AngularJS Mocks Backend v0.1.5
+  (c) 2014 Erko Bridee - https://github.com/erkobridee/angular-mocks-backend/releases/tag/0.1.5
   License: MIT
 */
 (function(angular) {
